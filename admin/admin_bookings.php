4adcc9f2a9e00836c6e3a26fe5857128c7241222
nitindantani <nitindantani764@gmail.com>
2025-06-10 11:57:30 +0530

    Update admin_bookings.php

--- admin/admin_bookings.php
+++ admin/admin_bookings.php
@@ -12,7 +12,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Connect to DB
-$conn = new mysqli("localhost", "root", "", "TouristBooking");
+$conn = new mysqli("sql12.freesqldatabase.com", "sql12783951", "AY3kzpvH9n", "sql12783951");
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
 // Fetch all bookings, ordering by ID
