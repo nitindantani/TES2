4bbbc010b110c9cb98ad63e0eee8ae89c29d3914
nitindantani <nitindantani764@gmail.com>
2025-06-10 11:53:31 +0530

    Update export_report.php

--- admin/export_report.php
+++ admin/export_report.php
@@ -15,20 +15,9 @@ if (!in_array($format, ['csv', 'pdf'])) {
 }
 
 // 2) DB query
-// PostgreSQL connection string
-$host = 'dpg-d0g4sbjuibrs73f8ot10-a.oregon-postgres.render.com';
-$port = '5432';
-$dbname = 'touristbooking';
-$user = 'touristbooking_user';
-$password = 'QbFGlPz2ytIxmfJdHSkaeO3BCSu7HBMl';
-
-// Establish connection
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
-} else {
-    echo "Connection successful!";
+$db = new mysqli('localhost', 'root', '', 'scanner');
+if ($db->connect_error) {
+    die("DB connection failed: " . $db->connect_error);
 }
 
 $stmt = $db->prepare("
