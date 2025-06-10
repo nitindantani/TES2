20cfc693f7a97e02becbbf8ec474aa71655fbaae
nitindantani <nitindantani764@gmail.com>
2025-06-10 10:56:44 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,13 @@
 <?php
 // Database connection
-// PostgreSQL connection string
-$host = 'dpg-d0g4sbjuibrs73f8ot10-a.oregon-postgres.render.com';
-$port = '5432';
-$dbname = 'touristbooking';
-$user = 'touristbooking_user';
-$password = 'QbFGlPz2ytIxmfJdHSkaeO3BCSu7HBMl';
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "TouristBooking";
 
-// Establish connection
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
-} else {
-    echo "Connection successful!";
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $identifier = $_POST['identifier'];  // Email or mobile
