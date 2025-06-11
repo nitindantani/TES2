327b35a9d1bbdfb4995a99ca9d1c270aa921dd09
nitindantani <nitindantani764@gmail.com>
2025-06-11 09:44:01 +0530

    Update scan.php

--- admin/scan.php
+++ admin/scan.php
@@ -1,7 +1,7 @@
 <?php
-$scanner_conn = new mysqli("localhost", "root", "", "scanner");
+$scanner_conn = new mysqli("sql12.freesqldatabase.com", "sql12784142", "IgcSQrkBtC", "sql12784142");
 
-$date = date('d-m-Y'); // today
+$date = date('Y-m-d'); // today
 $result = $scanner_conn->query("SELECT COUNT(*) as total FROM booking WHERE DATE(scanned_at) = '$date'");
 $row = $result->fetch_assoc();
 
