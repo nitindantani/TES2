35f0b0e26e0c8f6bbd0af9406a32f8120777aa18
nitindantani <nitindantani764@gmail.com>
2025-06-26 16:44:36 +0530

    Update export_csv.php

--- admin/export_csv.php
+++ admin/export_csv.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('sql12.freesqldatabase.com','sql5786934','m3wWsIZkxF','sql5786934');
+$conn = new mysqli('sql5.freesqldatabase.com','sql5786934','m3wWsIZkxF','sql5786934');
 
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment;filename="tourist_bookings.csv"');
