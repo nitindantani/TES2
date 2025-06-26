b1da31cc94adb4386a09212dc1203d9679c23d23
nitindantani <nitindantani764@gmail.com>
2025-06-26 16:21:06 +0530

    Update update_booking.php

--- admin/update_booking.php
+++ admin/update_booking.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
 
     // Update booking in the database
-    $conn = new mysqli("sql12.freesqldatabase.com", "sql12783951", "AY3kzpvH9n", "sql12783951");
+    $conn = new mysqli('sql12.freesqldatabase.com','sql5786934','m3wWsIZkxF','sql5786934');
     $sql = "UPDATE bookings SET name = ?, visit_date = ?, gender = ?, place = ?, nationality = ?, age = ?, total_tourists = ?, adults = ?, children = ?, mobile = ?, email = ? WHERE id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("sssssssssssi", $name, $visit_date, $gender, $place, $nationality, $age, $tourists, $adults, $children, $mobile, $email, $id);
