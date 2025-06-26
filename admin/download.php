c5ce5b931985ebc53ee664b8d5f171014f3d5b91
nitindantani <nitindantani764@gmail.com>
2025-06-26 16:19:43 +0530

    Update download.php

--- admin/download.php
+++ admin/download.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Connect to DB
-$conn = new mysqli("sql12.freesqldatabase.com", "sql12783951", "AY3kzpvH9n", "sql12783951");
+$conn = new mysqli('sql12.freesqldatabase.com','sql5786934','m3wWsIZkxF','sql5786934');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
